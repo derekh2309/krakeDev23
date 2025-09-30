ae36c05621c4f34497571a980a3f1444e89e1e79
derekh2309 <derek.h2309@gmail.com>
2025-09-29 23:04:20 -0500

    El boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/Ganar.gif"
     console.log("ingresa a saludar")
 }
