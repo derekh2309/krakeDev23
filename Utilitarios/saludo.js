6424de509ed7ba89f7761033dcd37fcad2b5f007
derekh2309 <derek.h2309@gmail.com>
2025-09-30 23:10:00 -0500

    mostrar textoEnCaja implementado

--- Utilitarios/saludo.js
+++ Utilitarios/saludo.js
@@ -13,6 +13,8 @@ saludar=function(){
     mostrarTexto("lblResultado", mensajeBienvenida);
 
     mostrarImagen("imgSaludo","./imagenes/saludo.gif");
+
+    mostrarTextoEnCaja("txtNombre","")
 }
 
 mostrarImagen=function(idComponente, rutaImagen){
@@ -33,6 +35,12 @@ mostrarTexto=function(idComponente, mensaje){
     let componente;
     componente=document.getElementById(idComponente);
     componente.innerText=mensaje;
+}
+
+mostrarTextoEnCaja=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.value=mensaje;
 }
 
 recuperarInt=function(idComponente){
