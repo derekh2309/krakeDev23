9423ab3c3a87315d84b69d851affcd9fc887489b
derekh2309 <derek.h2309@gmail.com>
2025-10-11 18:02:01 -0500

    Imagenes funcionando a la perfección

--- PromedioNotas/promedioNotas.js
+++ PromedioNotas/promedioNotas.js
@@ -3,7 +3,14 @@ calcularPromedioNotas=function(){
     let nota2 = parseFloat(document.getElementById("txtNota2").value);
     let nota3 = parseFloat(document.getElementById("txtNota3").value);
     let promedio = calcularPromedio(nota1, nota2, nota3);
+    let img = document.getElementById("imgResultado")
     document.getElementById("resultado").innerText = "El promedio es: " + promedio.toFixed(2);
+    if (promedio > 7){
+        img.src ="./imagenes/Aprobado.gif";
+    }
+    else{
+        img.src = "./imagenes/Reprobado.gif";
+    }
 }
 
 calcularPromedio=function(nota1,nota2,nota3){
